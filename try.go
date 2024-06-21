445afb94b35e7ec1af115d412a188322b8f0a99e
caiies <jxii3d@outlook.com>
2024-06-21 11:27:20 +0800

    Modify try.go

--- try.go
+++ try.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Authority become visit serve ever country rule.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
