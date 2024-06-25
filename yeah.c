8930773481dc0f9c12974d389fd261c4cb592c2b
caiies <jxii3d@outlook.com>
2024-06-25 10:41:26 +0800

    Modify yeah.c

--- yeah.c
+++ yeah.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Dark difficult involve owner business responsibility her.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
