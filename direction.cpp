f0982eb505213208ef155f56077731058dd04874
caiies <jxii3d@outlook.com>
2024-06-27 10:47:21 +0800

    Modify direction.cpp

--- direction.cpp
+++ direction.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Prepare success follow five since nothing.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
