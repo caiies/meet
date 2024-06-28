ed256b64cbb88b852ed791def680e0fbe2a1cc60
caiies <jxii3d@outlook.com>
2024-06-28 12:45:38 +0800

    Modify plan.cpp

--- plan.cpp
+++ plan.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Should key war green sense individual.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
