80c35dcd05bac12bd150a179ffa69a56a94292be
caiies <jxii3d@outlook.com>
2024-07-11 18:08:22 +0800

    Modify direction.cpp

--- direction.cpp
+++ direction.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Discuss relate serious line everyone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
