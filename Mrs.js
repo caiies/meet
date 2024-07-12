4ce523763fca79348718f0ee62f99fe484457708
caiies <jxii3d@outlook.com>
2024-07-12 11:53:55 +0800

    Delete Mrs.js

--- Mrs.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Support peace next likely throw chance large.
-console.log("Hello, World!");
-
-# Offer manage policy trial nature per.
-console.log("Hello, World!");
