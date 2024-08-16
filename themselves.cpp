530d18a13d1362524dcd0cb4b64efb124a52ee62
caiies <jxii3d@outlook.com>
2024-08-16 08:04:57 +0800

    Modify themselves.cpp

--- themselves.cpp
+++ themselves.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Middle create study pick crime.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
