fd9bc5e36341c6615c7ea6d33ee3dc712e35693a
caiies <jxii3d@outlook.com>
2024-08-26 11:14:48 +0800

    Modify lead.c

--- lead.c
+++ lead.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# A make him section writer official modern ok.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
