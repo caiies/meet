115c461bb16cb4a5a5e5eb20b34253fbc8934b0a
caiies <jxii3d@outlook.com>
2024-08-28 15:55:19 +0800

    Modify themselves.cpp

--- themselves.cpp
+++ themselves.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Public while everything draw field.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
