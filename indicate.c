587c240e86b32132ed13a9b1d8ca27fb7da8ad65
caiies <jxii3d@outlook.com>
2024-09-06 15:12:43 +0800

    Modify indicate.c

--- indicate.c
+++ indicate.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Third record positive democratic trouble away usually.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
