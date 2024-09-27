047e0816c6d6c23ea6d0a8e7efc751af1144c293
caiies <jxii3d@outlook.com>
2024-09-27 12:44:41 +0800

    Modify indicate.c

--- indicate.c
+++ indicate.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Practice voice surface road necessary structure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
