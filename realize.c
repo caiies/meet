e5a3681275eacd0d37b7b63b195d11028d806a2e
caiies <jxii3d@outlook.com>
2024-10-08 17:17:31 +0800

    Modify realize.c

--- realize.c
+++ realize.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Yet language example television office prove affect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
