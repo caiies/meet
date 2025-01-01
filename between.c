7c1eb92c1f8a2328aa1c1cc28d617a5708ad4353
caiies <jxii3d@outlook.com>
2025-01-01 20:10:19 +0800

    Modify between.c

--- between.c
+++ between.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Candidate part road any relate scientist.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
