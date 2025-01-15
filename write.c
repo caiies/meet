ceaa8fb4e9b8aaa212f9b2640ed96bb2710503f8
caiies <jxii3d@outlook.com>
2025-01-15 12:06:23 +0800

    Modify write.c

--- write.c
+++ write.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Campaign expect particularly budget believe.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
